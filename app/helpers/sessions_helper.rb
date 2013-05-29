8609817daebd0fcdf96dd6a5e06e68b9ff32717c
Cintia Higashi <cintiamh@gmail.com>
2013-05-29 00:59:28 -0400

    fixed tests for sign in sign out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,11 +1,11 @@
 module SessionsHelper
   def sign_in(user)
     cookies.permanent[:remember_token] = user.remember_token
-    @current_user = user
+    current_user = user
   end
 
   def signed_in?
-    !@current_user.nil?
+    !current_user.nil?
   end
 
   def current_user=(user)
@@ -13,7 +13,7 @@ module SessionsHelper
   end
 
   def current_user
-    @current_user ||= User.find_by(remember_token: cookies[:remember_token])
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
   end
 
   def destroy
@@ -22,7 +22,7 @@ module SessionsHelper
   end
 
   def sign_out
-    @current_user = nil
+    current_user = nil
     cookies.delete(:remember_token)
   end
 end
